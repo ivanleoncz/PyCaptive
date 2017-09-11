ed893b6d91d31b60103fd604a5be869087bd9c63
ivanlmj <ivanlmj@gmail.com>
2017-09-11 00:05:22 -0500

    adding request.remote_addr attribute

--- app.py
+++ app.py
@@ -6,8 +6,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def f_index():
-    header = request.headers
-    return "Flask Running!<br><br>Request Headers:<br>%s" % header
+    remote_ip = request.remote_addr
+    headers = request.headers
+    return "Flask Running!<br><br>Remote IP: %s<br>Request Headers: %s" % (remote_ip,headers)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1",port=14900)
