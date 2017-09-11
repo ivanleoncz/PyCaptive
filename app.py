e7b32b934f3f04ae7bc4a86040b0b53083e8de58
ivanlmj <ivanlmj@gmail.com>
2017-09-10 23:54:33 -0500

    adding Flask Requests

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def f_index():
-    return "Flask Running!"
+    header = request.headers
+    return "Flask Running!<br><br>Request Headers:<br>%s" % header
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1",port=14900)
