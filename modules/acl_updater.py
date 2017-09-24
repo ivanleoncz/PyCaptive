35fb2068819f701bbf3099ef598127e343d78a8f
ivanlmj <ivanlmj@gmail.com>
2017-09-24 00:50:31 -0500

    improving module structure
    - removing unecessary prints from functions
    - removing unecessary checks from functions
    - creating check_acls() function
    - creating check_file() function
    - removing acls existence check statements (new check_acls())
    - removing file existenece check statements (new check_file())
    - improving output of messages
    - improving comments

--- modules/acl_updater.py
+++ modules/acl_updater.py
@@ -5,11 +5,14 @@ import os
 from subprocess import call
 import sys
 
+
+
 def add_ip(acl_file, ip):
     with open(acl_file, "r+") as f:
         f.seek(0,2)
         f.write(ip + "\n")
     return "Done!"
+
 
 def del_ip(acl_file, ip):
     with open(acl_file,"r+") as f:
@@ -21,23 +24,35 @@ def del_ip(acl_file, ip):
                 f.write(line)
     return "Done!"
 
+
 def read_acl(acl_file):
     with open(acl_file,"r") as f:
-        print("----------------")
         for line in f.readlines():
             print(line,end="")
-        print("\n----------------")
     return "Done!"
+
 
 def list_acls():
     acls = glob("*.acl")
-    if len(acls) == 0:
-        print("NO ACLs WERE FOUND!")
-    else:
-        print("\n[ACLs]")
-        for acl in acls:
-            print("-",acl,end="")
+    for acl in acls:
+        print("-",acl,end="")
     return "Done!"
+
+
+def check_acls():
+    acls = glob("*.acl")
+    if len(acls) != 0:
+        return "ok"
+    else:
+        return "No ACLs were found!"
+
+
+def check_file(acl):
+    if os.path.isfile(acl):
+        return "ok"
+    else:
+        return "ACL not found!"
+
 
 try:
     while True:
@@ -50,82 +65,98 @@ try:
         print("3. List")
         print("4. Exit\n")
         opt = input("Option: ")
-        # modify ACL
+
+        # MODIFY
         if opt == "1":
             flag = "0"
             while flag == "0":
                 call(['clear'])
                 print("\n\n\n$ MODIFY\n")
-                acls = glob("*.acl")
-                if len(acls) == 0:
-                    print("NO ACLs WERE FOUND!")
-                    input("Press any key to continue...\n")
-                    flag = "1"
-                else:
+                check = check_acls()
+                if check == "ok":
                     print("1. Add IP")
                     print("2. Del IP")
                     print("3. Exit\n")
                     oper = input("Option: ")
-
-                    # add IP
+                    # > add IP
                     if oper == "1":
                         list_acls()
                         acl = input("\n\nACL Name: ")
-                        if os.path.isfile(acl):
+                        check_acl = check_file(acl) 
+                        if check_acl == "ok":
+                            print("----------------")
                             read_acl(acl)
-                            ip = input("\nAdd IP: ")
+                            print("\n----------------\n")
+                            ip = input("Add IP: ")
                             add_ip(acl,ip)
+                            print("----------------")
                             read_acl(acl)
-                            input("\nPress any key to continue...\n")
+                            print("\n----------------\n")
+                            input("Press any key to continue...\n")
                         else:
                             print("ACL NOT FOUND!\n")
                             input("Press any key to continue...\n")
-
-                    # del IP
+                    # > del IP
                     elif oper == "2":
                         list_acls()
                         acl = input("\n\nACL Name: ")
-                        if os.path.isfile(acl):
+                        check_acl = check_file(acl)
+                        if check_acl == "ok":
+                            print("----------------")
                             read_acl(acl)
-                            ip = input("\nDel IP: ")
+                            print("\n----------------\n")
+                            ip = input("Del IP: ")
                             del_ip(acl,ip)
+                            print("----------------")
                             read_acl(acl)
-                            input("\nPress any key to continue...\n")
+                            print("\n----------------\n")
+                            input("Press any key to continue...\n")
                         else:
                             print("ACL NOT FOUND!\n")
                             input("Press any key to continue...\n")
-
-                    # exit IP
+                    # > exit
                     elif oper == "3":
                         flag = "1"
                     else:
                         print("WRONG OPTION!")
-        # read ACL
+                else:
+                    print(check)
+
+        # READ
         elif opt == "2":
             call(['clear'])
             print("\n\n\n$ READ\n")
-            acls = glob("*.acl")
-            if len(acls) == 0:
-                print("NO ACLs WERE FOUND!\n")
-                input("Press any key to continue...\n")
-            else:
+            check = check_acls()
+            if check == "ok":
                 list_acls()
                 acl = input("\n\nACL Name: ")
-                if os.path.isfile(acl):
+                check_acl = check_file(acl)
+                if check_acl == "ok":
+                    print("----------------")
                     read_acl(acl)
-                    input("\n\nPress any key to continue...\n")
+                    print("\n----------------\n")
+                    input("Press any key to continue...\n")
                 else:
-                    print("ACL NOT FOUND!")
-                    input("\nPress any key to continue...\n")
+                    print("ACL NOT FOUND!\n")
+                    input("Press any key to continue...\n")
+            else:
+                print(check)
 
-        # list ACLs
+        # LIST
         elif opt == "3":
             call(['clear'])
             print("\n\n\n$ LIST\n")
-            list_acls()
-            input("\n\nPress any key to continue...\n")
+            check = check_acls()
+            if check == "ok":
+                print("----------------")
+                list_acls()
+                print("\n----------------\n")
+                input("Press any key to continue...\n")
+            else:
+                print(check)
+                input("Press any key to continue...\n")
 
-        # exit...
+        # EXIT
         elif opt == "4":
             print("\nBye!\n")
             sys.exit(0)
