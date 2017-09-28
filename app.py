236fc968329b1cafe9613aa4e0ac6cdb27d1b40a
ivanlmj <ivanlmj@gmail.com>
2017-09-28 00:16:30 -0500

    prototyping "/login" route

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request
+from flask import abort, Flask, request
 
 app = Flask(__name__)
 
@@ -9,6 +9,19 @@ def f_index():
     remote_ip = request.remote_addr
     headers = request.headers
     return "Flask Running!<br><br>Remote IP: %s<br>Request Headers: %s" % (remote_ip,headers)
+
+@app.route("/login", methods=['GET','POST'])
+def f_login():
+    if request.method == 'GET':
+        return "Welcome to login page!"
+    elif request.method == 'POST':
+        name = request.form['username']
+        passwd = request.form['password']
+        return "Processing login!"
+    else:
+        # 405: Method Not Allowed
+        abort(405)
+
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1",port=14900)
