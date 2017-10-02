5d4ac60c6dc77693d9c69f1fab785ce28e49415e
ivanlmj <ivanlmj@gmail.com>
2017-10-02 13:56:48 -0500

    adding render_template for '/login' route

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import abort, Flask, request
+from flask import abort, Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -13,7 +13,7 @@ def f_index():
 @app.route("/login", methods=['GET','POST'])
 def f_login():
     if request.method == 'GET':
-        return "Welcome to login page!"
+        return render_template("login.html")
     elif request.method == 'POST':
         name = request.form['username']
         passwd = request.form['password']
@@ -21,7 +21,6 @@ def f_login():
     else:
         # 405: Method Not Allowed
         abort(405)
-
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1",port=14900)
