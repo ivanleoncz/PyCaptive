8e2127def72702fba253f5e7f07035e93abebf01
ivanlmj <ivanlmj@gmail.com>
2017-10-06 15:14:27 -0500

    improvements on PyCaptive main module
    - removing bcrypt hash/unhash password test method
    - adding comments for functions
    - improving if/else decision based on Connector().login() return

--- app.py
+++ app.py
@@ -1,43 +1,39 @@
 #!/usr/bin/env python3
 
-import bcrypt
-from getpass import getpass
 from flask import abort, Flask, render_template, request
 from app_modules import mongodb
 
 app = Flask(__name__)
 
-def password_check(passwd):
-    salt = bcrypt.gensalt()
-    password = "Yougotit*88".encode('utf-8')
-    hashed = bcrypt.hashpw(password,salt)
-    new_hashed = bcrypt.hashpw(passwd.encode('utf-8'),hashed)
-    if new_hashed == hashed:
-        return "ok"
-
 @app.route("/")
 def f_index():
+    """ Index route for test purposes."""
     remote_ip = request.remote_addr
     headers = request.headers
     return "Flask Running!<br><br>Remote IP: %s<br>Request Headers: %s" % (remote_ip,headers)
 
 @app.route("/login", methods=['GET','POST'])
 def f_login():
+    """ Processing request. 
+        
+        GET: loading login interface
+        POST: processing login request (form data)
+    """
     if request.method == 'GET':
         return render_template("login.html")
     elif request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        print("POST Username:",username)
-        print("POST Password:",password)
         mc = mongodb.Connector(username,password)
         login = mc.login()
-        if login == "ok":
-            return "<h1> Login Successful!!! </h1>"
-        elif login == "nok":
-            return "<h1> Wrong Password... </h1>"
+        if login == 0:
+            return "<h1> Login Successful! </h1>"
+        elif login == 1 or login == 2:
+            return render_template("login.html",login_failed="Check Your Credentials...")
+        elif login == 2:
+            return "<h1> Database Error... </h1>"
         else:
-            return "<h1> Database Timeout! </h1>"
+            return "<h1> Database Module Error... </h1>"
     else:
         # 405: Method Not Allowed
         abort(405)
