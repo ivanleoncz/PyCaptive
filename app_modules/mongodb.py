d51e5d96133f6f442701707ec6fb50988818d403
ivanlmj <ivanlmj@gmail.com>
2017-10-11 16:28:13 -0500

    adding login_record() - prototyping

--- app_modules/mongodb.py
+++ app_modules/mongodb.py
@@ -2,6 +2,7 @@
 """  MongoDB utilities for PyCaptive."""
 
 import bcrypt
+from datetime import datetime,timedelta
 from pymongo import MongoClient
 
 class Connector:
@@ -24,6 +25,19 @@ class Connector:
         uri = "mongodb://{0}:{1}@127.0.0.1:27017".format(self.db_user,self.db_pass)
         self.client = MongoClient(uri,serverSelectionTimeoutMS=6000)
 
+    def login_record(self,db):
+        """ Recording successful login attempts. """
+        col = db["AuthenticationTempRecords"]
+        login = datetime.now()
+        expire_time = login + timedelta(hours=3)
+        login = str(login).split(".")[0]
+        expire_time = str(expire_time).split(".")[0]
+        insert_record = col.insert_one({"Username":self.username,"IpAddress":self.ip,"LoginTime":login,"ExpireTime":expiretime})
+        if insert_record:
+            return "ok"
+        else:
+            return "nok"
+
     def login(self):
         """ Connecting and searching for correct username and password.
 
@@ -40,7 +54,12 @@ class Connector:
                 hash_pass = hash_pass["Password"].encode("utf-8")
                 unhashed_pass = bcrypt.hashpw(self.password.encode('utf-8'),hash_pass)
                 if hash_pass == unhashed_pass:
-                    return 0
+                    # firewall rule must apply here (or not ???...)
+                    record = login_record()
+                    if record == "ok":
+                        return 0
+                    else:
+                        return 200
                 else:
                     return 1
             else:
