1ea2c2f46996d518609036082225cee2ecdc3215
ivanlmj <ivanlmj@gmail.com>
2017-10-12 17:29:20 -0500

    improving module structure and fixing bugs
    - adding "ip" variable to class constructor
    - defining error codes
    - cleaning comments from methods
    - defining try/except for login_record() execution
    - removing firewall jobs (moving to main app module)
    - adjusting if/else to new error codes

--- app_modules/mongodb.py
+++ app_modules/mongodb.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
-"""  MongoDB utilities for PyCaptive."""
+
+"""  MongoDB utilities for PyCaptive.
+
+     Return Codes:
+
+     0x0000 - Successful
+     0x0db1 - Exception
+     0x0db2 - User Not Found
+     0x0db3 - Wrong Password
+     0x0db4 - Login Record Failed
+"""
 
 import bcrypt
 from datetime import datetime,timedelta
@@ -14,8 +24,9 @@ class Connector:
     client = None
     username = None
     password = None
+    ip = None
 
-    def __init__(self,username,password):
+    def __init__(self,username,password,ip):
         """ Storing username and password from Flask (POST)."""
         self.username = username
         self.password = password
@@ -27,27 +38,20 @@ class Connector:
 
     def login_record(self,db):
         """ Recording successful login attempts. """
-        col = db["AuthenticationTempRecords"]
-        login_time = datetime.now()
-        expire_time = login_time + timedelta(hours=3)
-        login_time = str(login).split(".")[0]
-        expire_time = str(expire_time).split(".")[0]
-        insert_record = col.insert_one({"Username":self.username,"IpAddress":self.ip,"LoginTime":login_time,"ExpireTime":expire_time})
-        if insert_record:
-            return "ok"
-        else:
-            return "nok"
+        try:
+            col = db["AuthenticationTempRecords"]
+            login_time = datetime.now()
+            expire_time = login_time + timedelta(hours=3)
+            login_time = str(login).split(".")[0]
+            expire_time = str(expire_time).split(".")[0]
+            insert_record = col.insert_one({"Username":self.username,"IpAddress":self.ip,"LoginTime":login_time,"ExpireTime":expire_time})
+            if insert_record:
+                return "0x0000"
+        except Exception:
+            return "0x0db1"
 
     def login(self):
-        """ Connecting to MongoDB, validating username/password + other functions.
-
-        0 - Login Process (Validation/Record/Firewall) Ok!
-        1 - User Not Found!
-        2 - Wrong Password!
-        3 - Login Record Failed!
-        4 - Firewall Rule Failed!
-        5 - Exception!
-        """
+        """ Connecting to MongoDB, validating username/password + other functions. """
         try:
             self.connect()
             db = self.client["tjs"]
@@ -58,19 +62,14 @@ class Connector:
                 unhashed_pass = bcrypt.hashpw(self.password.encode('utf-8'),hash_pass)
                 if hash_pass == unhashed_pass:
                     record = login_record()
-                    if record == "ok":
-                        firewall = test_rule()
-                        if firewall == "ok":
-                            return 0
-                        else:
-                            return 4
+                    if record == "0x0000":
+                        return "0x0000"
                     else:
-                        return 3
+                        return "0x0db4"
                 else:
-                    return 2
+                    return "0x0db3"
             else:
-                return 1
-        except Exception as e:
-            # print("Exception:",e)
-            return 5
+                return "0x0db2"
+        except Exception:
+            return "0x0db1"
 
