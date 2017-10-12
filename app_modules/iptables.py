34b9173e8b254fe66432e23640ff5b34a5e6c974
ivanlmj <ivanlmj@gmail.com>
2017-10-12 17:43:54 -0500

    improving module structure + adding test methods
    - improving module comments
    - adding new error code definition
    - adding methods for test purposes
    - improving try/except, according to new error code definition

--- app_modules/iptables.py
+++ app_modules/iptables.py
@@ -1,5 +1,13 @@
+#!/usr/bin/env python3
 
-""" Interface for IPTABLES/Netfilter. """
+""" Interface for IPTABLES/Netfilter. 
+    
+    Return Codes:
+
+    0x0000 - Successful
+    0x0fw1 - Exception
+    0x0fw2 - Not Found
+"""
 
 import iptc
 
@@ -10,6 +18,47 @@ class Ruler:
 
     def __init__(self,ip):
         self.ip = ip
+
+    def test_add_rule(self):
+        """ Test Rule (add). """
+        try:
+            table = iptc.Table(iptc.Table.FILTER)
+            chain = iptc.Chain(table, "INPUT")
+            rule = iptc.Rule()
+            rule.src = self.ip
+            rule.protocol = "tcp"
+            rule.in_interface =  "lo"
+            rule.target = rule.create_target("DROP")
+            match = rule.create_match("tcp")
+            match.dport = "27017"
+            chain.insert_rule(rule)
+            return "0x0000"
+        except Exception:
+            return "0x0fw1"
+
+    def test_del_rule(self):
+        """ Test Rule (del). """
+        try:
+            table = iptc.Table(iptc.Table.FILTER)
+            chain = iptc.Chain(table, "INPUT")
+            table.autocommit = False
+            deleted = False
+            while deleted == False:
+                for rule in chain.rules:
+                    if rule.src == self.ip and rule.in_interface is not None and "lo" in rule.in_interface:
+                        for match in rule.matches:
+                            if match.dport == "27017":
+                                chain.delete_rule(rule)
+                                deleted = True
+            table.commit()
+            table.autocommit = True
+            if deleted == True:
+                return "0x0000"
+            else:
+                return "0x0fw2"
+        except Exception:
+            return "0x0fw1"
+
 
     def add_rule(self):
         """ Adding rule. """
@@ -25,9 +74,9 @@ class Ruler:
             match = rule.create_match('tcp')
             match.dport = "80"
             chain.insert_rule(rule)
-            return "ok"
-        except Exception as e:
-            return e
+            return "0x0000"
+        except Exception:
+            return "0x0fw1"
 
     def del_rule(self):
         """ Deleting rule. """
@@ -38,6 +87,6 @@ class Ruler:
                 if rule.src == self.ip:
                     chain.delete_rule(rule)
                     break
-            return "ok"
-        except Exception as e:
-            return e
+            return "0x0000"
+        except Exception:
+            return "0x0fw1"
