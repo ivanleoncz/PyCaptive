8bce95b96806e9ffb4c419d7d46f6e4c6a7548e8
ivanlmj <ivanlmj@gmail.com>
2017-10-18 23:08:18 -0500

    restructuting module
    - adding return code (comment)
    - removing class unnecessary variables
    - separating login_record() in two methods
    - removing unnecessary variable for login()
    - removing unnecessary class variable assignments
    - fixing variable for PyMongo query
    - removing login_record() method calling
    - adding print statements for temporal debug

--- app_modules/mongodb.py
+++ app_modules/mongodb.py
@@ -5,11 +5,12 @@
      Return Codes:
 
      0x0000 - Successful
-     0x0db1 - Fail To Execute Query
+     0x0db1 - Fail To Process Login
      0x0db2 - User Not Found
      0x0db3 - Wrong Password
      0x0db4 - Fail To Add Record
      0x0db5 - Fail To Del Record
+     0x0db6 - Operation Not Found
 """
 
 import bcrypt
@@ -18,65 +19,65 @@ from pymongo import MongoClient
 
 class Connector:
     """ MongoDB connector. """
-    db = None
     client = None
-    username = None
-    ipaddress = None
 
     def connect(self):
-        """ Preparing Client. """
+        """ Preparing Client """
         db_user = "mongo"
         db_pass = "mongo"
         uri = "mongodb://{0}:{1}@127.0.0.1:27017".format(db_user,db_pass)
         self.client = MongoClient(uri,serverSelectionTimeoutMS=6000)
 
-    def login_record(self,oper):
-        """ Login Record Tasks. """
-        collection = self.db.AuthenticationTempRecords
-        if oper == "add":
-            try:
-                login_time = datetime.now()
-                expire_time = login_time + timedelta(hours=3)
-                collection.insert_one({"Username":self.username,"IpAddress":self.ipaddress"LoginTime":login_time,"ExpireTime":expire_time})
-                return "0x0000"
-            except Exception as e:
-                return "0x0db4"
-        elif oper == "del":
-            try:
-                time_now = datetime.now()
-                time_now = str(time_now).split(".")[0]
-                expired_sessions = collection.find().distinct("ExpireTime")
-                for session in expired_sessions:
-                    if session < time_now:
-                        collection.delete_one({"Username":self.username})    
-                return "0x0000"
-            except Exception as e:
-                return "0x0db5"
-        else:
-            return "Wrong Option!"
+    def add_record(self,username,ipaddress):
+        """ Adding Login Record """
+        self.connect()
+        db = self.client.tjs
+        collection = db.AuthenticationTempRecords
+        try:
+            login_time = datetime.now()
+            expire_time = login_time + timedelta(hours=3)
+            collection.insert_one({"Username":username,"IpAddress":ipaddress,"LoginTime":login_time,"ExpireTime":expire_time})
+            return "0x0000"
+        except Exception:
+            print("ERROR: fail TO ADD login record...")
+            return "0x0db4"
+
+    def del_record(self):
+        """  Deleting Login Record """
+        self.connect()
+        db = self.client.tjs
+        collection = db.AuthenticationTempRecords
+        try:
+            time_now = datetime.now()
+            time_now = str(time_now).split(".")[0]
+            expired_sessions = collection.find().distinct("ExpireTime")
+            for session in expired_sessions:
+                if session < time_now:
+                    collection.delete_one({"ExpireTime":session})    
+            return "0x0000"
+        except Exception:
+            print("ERROR: fail TO DEL login record...")
+            return "0x0db5"
             
-    def login(self,username,password,ipaddress):   
+    def login(self,username,password):   
         """ Connecting to MongoDB, validating username/password + other functions. """
-        self.username = username
-        self.ipaddress = ipaddress
         try:
             self.connect()
-            self.db = self.client.tjs
-            collection = self.db.Authentication
-            hash_pass = collection.find_one({"Username":self.username},{"Password":1,"_id":0})
+            db = self.client.tjs
+            collection = db.Authentication
+            hash_pass = collection.find_one({"Username":username},{"Password":1,"_id":0})
             if hash_pass:
                 hash_pass = hash_pass["Password"].encode("utf-8")
                 unhashed_pass = bcrypt.hashpw(password.encode('utf-8'),hash_pass)
                 if hash_pass == unhashed_pass:
-                    record = self.login_record("add")
-                    if record == "0x0000":
-                        return "0x0000"
-                    else:
-                        return "0x0db4"
+                    return "0x0000"
                 else:
+                    print("ERROR: wrong password...")
                     return "0x0db3"
             else:
+                print("ERROR: user NOT FOUND...")
                 return "0x0db2"
         except Exception as e:
+            print("ERROR: fail TO PROCESS login...\n",e)
             return "0x0db1"
 
