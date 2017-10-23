518f413f977d49265f55f0edc19bbcfe072fd5bd
ivanlmj <ivanlmj@gmail.com>
2017-10-22 20:59:27 -0500

    fixing bugs + improvements
    - removing class constructor
    - removing class variable
    - adding variable to test_add_rule()
    - removing assignment of class variables
    - adding deleted_rules counter
    - removing while loop
    - removing class variable from if/else
    - removing unecessary if
    - adding comments
    - adding varialbles to methods
    - improving print Exceptions for temporal debugs

--- app_modules/iptables.py
+++ app_modules/iptables.py
@@ -6,26 +6,20 @@
 
     0x0000 - Successful
     0x0fw1 - fail to add firewall rule
-    0x0fw2 - firewall rule not found
 """
 
 import iptc
 
-class Ruler:
+class Worker:
     """ IPTABLES/Netfilter jobs."""
 
-    ipaddress = None
-
-    def __init__(self,ipaddress):
-        self.ipaddress = ipaddress
-
-    def test_add_rule(self):
+    def test_add_rule(self,ip):
         """ Test Rule (add). """
         try:
             table = iptc.Table(iptc.Table.FILTER)
             chain = iptc.Chain(table, "INPUT")
             rule = iptc.Rule()
-            rule.src = self.ipaddress
+            rule.src = ip
             rule.protocol = "tcp"
             rule.in_interface =  "lo"
             rule.target = rule.create_target("DROP")
@@ -33,42 +27,39 @@ class Ruler:
             match.dport = "9999"
             chain.insert_rule(rule)
             return "0x0000"
-        except Exception:
-            print("ERROR: fail TO ADD firewall rule...")
+        except Exception as e:
+            print("ERROR: fail TO ADD firewall rule:", e)
             return "0x0fw1"
 
-    def test_del_rule(self):
+    def test_del_rule(self,ips):
         """ Test Rule (del). """
         try:
             table = iptc.Table(iptc.Table.FILTER)
             chain = iptc.Chain(table, "INPUT")
             table.autocommit = False
-            deleted = False
-            while deleted == False:
+            deleted_rules = 0
+            for ip in ips: 
                 for rule in chain.rules:
-                    if rule.src == self.ipaddress and rule.in_interface is not None and "lo" in rule.in_interface:
+                    if rule.src == ip and rule.in_interface is not None and "lo" in rule.in_interface:
                         for match in rule.matches:
                             if match.dport == "9999":
                                 chain.delete_rule(rule)
-                                deleted = True
+                                deleted_rules += 1
             table.commit()
             table.autocommit = True
-            if deleted == True:
-                return "0x0000"
-            else:
-                return "0x0fw2"
-        except Exception:
-            print("ERROR: fail TO DEL firewall rule...")
+            return deleted_rules
+        except Exception as e:
+            print("ERROR: fail TO DEL firewall rule:",e)
             return "0x0fw1"
 
-
-    def add_rule(self):
+    # ---> need review
+    def add_rule(self,ip):
         """ Adding rule. """
         try:
             table = iptc.Table(iptc.Table.NAT)
             chain = iptc.Chain(table, "PREROUTING")
             rule = iptc.Rule()
-            rule.src = self.ipaddress
+            rule.src = ip
             rule.protocol = "tcp"
             rule.in_interface = "eth2"
             rule.target = rule.create_target("DROP")
@@ -78,16 +69,17 @@ class Ruler:
             chain.insert_rule(rule)
             return "0x0000"
         except Exception:
-            print("ERROR: fail TO DEL firewall rule...")
+            print("ERROR: fail TO ADD firewall rule...")
             return "0x0fw1"
 
-    def del_rule(self):
+    # ---> need review
+    def del_rule(self,ip):
         """ Deleting rule. """
         try:
             table = iptc.Table(iptc.Table.NAT)
             chain = iptc.Chain(table, "PREROUTING")
             for rule in chain.rules:
-                if rule.src == self.ipaddress:
+                if rule.src == ip:
                     chain.delete_rule(rule)
                     break
             return "0x0000"
