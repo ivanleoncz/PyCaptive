e7018a7080a82d700b9fea64286f0b647a6cd281
ivanlmj <ivanlmj@gmail.com>
2017-11-05 13:35:54 -0600

    adding comments for better documentation

--- app/modules/logger.py
+++ app/modules/logger.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 
+""" Module for logging configuration. """
+
 import logging
 from logging.handlers import RotatingFileHandler
 
 class Logger:
+    """ Logger configuration. """
 
     def config(self):
+        """ Setting up. """
         conf = logging.getLogger('__name__')
-        if not conf.handlers:
+        if not conf.handlers: # this if, avoid multiple execution of log messages
             conf.setLevel(logging.ERROR)
             file_path = "/var/log/captive_portal.log"
             handler = RotatingFileHandler(file_path, maxBytes=50000000, backupCount=5)
