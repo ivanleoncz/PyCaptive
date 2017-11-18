4fdd8622946c2959e4311a462b02ba124e26e915
ivanlmj <ivanlmj@gmail.com>
2017-11-18 16:50:50 -0600

    running PyCaptive on all IPs

--- run.py
+++ run.py
@@ -12,5 +12,5 @@ if __name__ == "__main__":
     sched = BackgroundScheduler(daemon=True)
     sched.add_job(scheduler.ses,'interval',seconds=60)
     sched.start()
-    app.run(host="127.0.0.1",port=14900)
+    app.run(host="0.0.0.0",port=14900)
     
