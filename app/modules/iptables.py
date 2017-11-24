c494e93cbf2bc351af7f9f5a052c3e0d5def87b0
ivanlmj <ivanlmj@gmail.com>
2017-11-24 16:48:32 -0600

    improving module structure
    - removing temporal "prints"
    - adding log messages
    - improving code breaklines
    - improving docstrings

--- app/modules/iptables.py
+++ app/modules/iptables.py
@@ -2,76 +2,81 @@
 
 """ Interface for IPTABLES/Netfilter. """
 
+from datetime import datetime
 import subprocess as sp
 
+from app import log
+
 class Worker:
-    """ IPTABLES/Netfilter jobs."""
+    """ Builds and executes IPTABLES/Netfilter rules (add/del)."""
 
     def test_add_rule(self,ip):
-        """ Test Purposes: Add Rule """
+        """ Add Rule (TEST PURPOSES). """
         try:
-            print("IPTABLES: test_add_rule() has been called.")
             r = sp.call(['/sbin/iptables', '-A', 'INPUT', '-i', 'lo', 
-                                                '-s', ip, '-p', 'tcp', '--dport', '10800', '-j', 'DROP'])
+                                           '-s', ip, '-p', 'tcp', 
+                                           '--dport', '10800', '-j', 'DROP'])
             if r == 0:
-                print("IPTABLES: firewall rule has been ADDED.")
+                log.error('%s %s %s', datetime.now(), "IPTABLES", "EVENT:[Adding Rule TEST] OK")
                 return 0
             else:
-                print("IPTABLES: fail to ADD firewall rule.")
-                return 1
+                log.error('%s %s %s', datetime.now(), "IPTABLES", "EVENT:[Adding Rule TEST] NOK")
+                return 4
         except Exception as e:
-            return "ERROR: %s" % e
+            log.error('%s %s %s %s', datetime.now(), "IPTABLES", "EVENT:[Exception TEST]", e)
+            return e
 
 
     def test_del_rule(self,ips):
-        """ Test Purposes; Delete Rules """
+        """ Delete rule (TEST PURPOSES). """
         try:
-            print("IPTABLES: test_add_rule() has been called.")
             rules = 0
             for ip in ips:
                 r = sp.call(['/sbin/iptables', '-D', 'INPUT', '-i', 'lo', 
-                                                    '-s', ip, '-p', 'tcp', '--dport', '10800', '-j', 'DROP'])
+                                               '-s', ip, '-p', 'tcp', 
+                                               '--dport', '10800', '-j', 'DROP'])
                 if r == 0:
-                    print("IPTABLES: firewall rule has been DELETED.")
+                    log.error('%s %s %s', datetime.now(), "IPTABLES", "EVENT:[Deleting Rule TEST] OK")
                     rules += 1
-                else:
-                    print("IPTABLES: fail to DELETE firewall rule.")
+                else:I
+                    log.error('%s %s %s', datetime.now(), "IPTABLES", "EVENT:[Deleting Rule TEST] NOK")
             return rules
         except Exception as e:
-            return "ERROR: %s" % e
+            log.error('%s %s %s %s', datetime.now(), "IPTABLES", "EVENT:[Exception TEST]", e)
+            return e
 
 
     def add_rule(self,ip):
-        """ Adding rule. """
+        """ Add rule. """
         try:
-            print("IPTABLES: test_add_rule() has been called.")
             r = sp.call(['/sbin/iptables', '-I', 'PREROUTING', '1', '-i', 'eth2', 
-                                                          '-s', ip, '-p', 'tcp', '--dport', '80', 
-                                                          '-j', 'DNAT', '--to-destination', '192.168.0.1:3128'])
+                                           '-s', ip, '-p', 'tcp', '--dport', '80', 
+                                           '-j', 'DNAT', '--to-destination', '192.168.0.1:3128'])
             if r == 0:
-                print("IPTABLES: firewall rule has been ADDED.")
+                log.error('%s %s %s', datetime.now(), "IPTABLES", "EVENT:[Adding Rule] OK")
                 return 0
             else:
-                print("IPTABLES: fail to ADD firewall rule.")
-                return 1
+                log.error('%s %s %s', datetime.now(), "IPTABLES", "EVENT:[Adding Rule] OK")
+                return "fail to add rule"
         except Exception as e:
-            return "ERROR: %s" % e
+            log.error('%s %s %s %s', datetime.now(), "IPTABLES", "EVENT:[Exception]", e)
+            return e
 
 
     def del_rule(self,ips):
-        """ Deleting rule. """
+        """ Delete rule. """
         try:
-            print("IPTABLES: test_add_rule() has been called.")
             rules = 0
             for ip in ips:
                 r = sp.call(['/sbin/iptables', '-I', 'PREROUTING', '-i', 'eth2', 
-                                                         '-s', ip, '-p', 'tcp', '--dport', '80', 
-                                                         '-j', 'DNAT', '--to-destination', '192.168.0.1:3128'])
+                                               '-s', ip, '-p', 'tcp', '--dport', '80', 
+                                               '-j', 'DNAT', '--to-destination', '192.168.0.1:3128'])
                 if r == 0:
-                    print("IPTABLES: firewall rule has been DELETED.")
+                    log.error('%s %s %s', datetime.now(), "IPTABLES", "EVENT:[Deleting Rule] OK")
                     rules += 1
                 else:
-                    print("IPTABLES: fail to DELETE firewall rule.")
+                    log.error('%s %s %s', datetime.now(), "IPTABLES", "EVENT:[Deleting Rule] NOK")
             return rules
         except Exception as e:
-            return "ERROR: %s" % e
+            log.error('%s %s %s %s', datetime.now(), "IPTABLES", "EVENT:[Exception]", e)
+            return e
