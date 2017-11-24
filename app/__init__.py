aaa0799d6b2b4dbf8609ed3acdd90fae72c91582
ivanlmj <ivanlmj@gmail.com>
2017-11-24 16:37:57 -0600

    configuring single log object + log every request
    - creating single object (configuration) for logging operations
    - setting/adding @app.after_request for logging every request

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,26 @@
 #!/usr/bin/python3
 
-from flask import Flask
+from datetime import datetime
+from flask import Flask, request
+
+from app.modules import logger
+
+log = logger.config()
 
 app = Flask(__name__)
+
+# setting up logging of every request, except Status Code 500 (see if below...)
+@app.after_request
+def after_request(response):
+    if response.status_code != 500:
+        timestamp = datetime.now()
+        log.error('%s %s %s %s %s %s',
+                   timestamp, 
+                   request.remote_addr, 
+                   request.method, 
+                   request.scheme, 
+                   request.full_path, 
+                   response.status)
+    return response
 
 from app.views import login
