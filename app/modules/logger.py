6b169aef7673b95dcac0f44468ed4b4cd208711f
ivanlmj <ivanlmj@gmail.com>
2017-11-24 16:49:43 -0600

    improving module structure
    - removing unnecessary class
    - configuring single function (more pragmatic)

--- app/modules/logger.py
+++ app/modules/logger.py
@@ -5,18 +5,12 @@
 import logging
 from logging.handlers import RotatingFileHandler
 
-class Logger:
-    """ Logger configuration. """
-
-    def config(self):
-        """ Setting up. """
-        conf = logging.getLogger('__name__')
-        if not conf.handlers: # this if, avoid multiple execution of log messages
-            conf.setLevel(logging.ERROR)
-            file_path = "/var/log/captive_portal.log"
-            handler = RotatingFileHandler(file_path, maxBytes=50000000, backupCount=5)
-            conf.addHandler(handler)
-        return conf
-
-
-
+def config(self):
+    """ Setting up log configuration. """
+    conf = logging.getLogger('__name__')
+    if not conf.handlers: # this if, avoid multiple execution of log messages
+        conf.setLevel(logging.ERROR)
+        file_path = "/var/log/captive_portal.log"
+        handler = RotatingFileHandler(file_path, maxBytes=50000000, backupCount=5)
+        conf.addHandler(handler)
+    return conf
