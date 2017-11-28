25da473eb0ae30b2cd692e9c215b993d1c85da2f
ivanlmj <ivanlmj@gmail.com>
2017-11-27 20:58:22 -0600

    improving scheduler module
    - changing scheduler function name
    - improving if/else for less log messages
    - improving log messages

--- app/modules/scheduler.py
+++ app/modules/scheduler.py
@@ -9,21 +9,23 @@ from app import log
 from app.modules import mongodb
 from app.modules import iptables
 
-def ses(): # Session Expiration Scheduler
+def expirer():
     """ Verifies and expires sessions. """
+    log.error('[%s] %s %s', datetime.now(), "EVENT", "scheduler expirer:RUNNING")
     db = mongodb.Connector()
-    sessions = mdbc.expire_sessions()
-    if type(sessions) == list and len(sessions) > 0:
-        log.error('%s %s %s %s', datetime.now(), "SCHEDULER", "EVENT:[Expired Sessions]", sessions)
-        fw = iptables.Worker()
-        counter = fw.test_del_rule(sessions)
-        if type(counter) == int and counter > 0:
-            log.error('%s %s %s %s', datetime.now(), "SCHEDULER",  "EVENT:[Removed Rules]", counter)
-        else:
-            log.error('%s %s %s %s', datetime.now(), "SCHEDULER", "EVENT:[Fail To Remove Rules]", counter)
+    sessions = db.expire_sessions()
+    if type(sessions) == list:
+        if len(sessions) > 0:
+            fw = iptables.Worker()
+            counter = fw.test_del_rule(sessions)
+            if type(counter) != int:
+                log.error('[%s] %s %s', datetime.now(), "EVENT", "scheduler expirer:FAIL_IPTABLES")
     else:
-        log.error('%s %s %s %s', datetime.now(), "SCHEDULER",  "EVENT:[No Session To Expire]", sessions)
+        log.error('[%s] %s %s', datetime.now(), "EVENT", "scheduler expirer:FAIL_MONGODB")
+
 
 sched = BackgroundScheduler(daemon=True)
-sched.add_job(ses,'interval',seconds=60)
+sched.add_job(expirer,'interval',seconds=60)
 sched.start()
+
+
