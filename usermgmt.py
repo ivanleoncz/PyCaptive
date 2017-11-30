491d3f9550000a254b39a06f801a405b2bd70d4e
ivanlmj <ivanlmj@gmail.com>
2017-11-29 22:24:16 -0600

    creating separate functions for tasks
    - function for password validation into a function - hashpass()
    - function for checking credentials
    - function for user creation
    - function for setting up database and collection

--- usermgmt.py
+++ usermgmt.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 
-import json
+import bcrypt
 from bson import json_util
+from datetime import datetime
+from getpass import getpass
+import json
 from pymongo import MongoClient
+import sys
 
 def connect():
     db_user = "mongo"
@@ -12,10 +16,76 @@ def connect():
     client = MongoClient(uri,serverSelectionTimeoutMS=6000)
     return client
 
-client = connect()
-db = client.tjs
-collection = db.Authentication
+def db_collection():
+    client = connect()
+    db = client.tjs
+    collection = db.Authentication
+    return collection
 
-username = collection.find_one({"Username":"ivan.leon"})
-data = json.dumps(username, indent=4, default=json_util.default)
-print(data)
+def hashpass(salt):
+    loop = 0
+    while loop == 0:
+        password = getpass(prompt="\n* Password: ").encode('utf-8')
+        password2 = getpass(prompt="* Retype:   ").encode('utf-8')
+        if password == password2:
+           loop = 1
+           passhash = bcrypt.hashpw(password, salt)
+           return passhash
+        else:
+           print("\n!!! Passwords are not the same !!! Try again!")
+
+
+def check_credentials(username):
+    print("\n[Checking Credentials]")
+    print("\nusername ->", username)
+    try:
+        query = db_collection().find_one({"Username":username},{"Password":1,"_id":0})
+        if query is not None:
+            salt_pass = query["Password"]
+            unhash_pass = hashpass(salt_pass)
+            if unhash_pass == salt_pass:
+                return "\nCorrect!\n"
+            else:
+                return "\nWrong Password!\n"    
+        else:
+            return "Username/Password Not Found!"
+    except (Exception,KeyboardInterrupt) as e:
+        return "\nInterrupted!"
+
+
+def create_user():
+    try:
+        print("\n[Creating User]\n")
+        salt = bcrypt.gensalt()
+        user_data = {}
+        user_data["Name"]         = input("* Full Name:  ")
+        user_data["Sector"]       = input("* Sector:     ")
+        user_data["Email"]        = input("* Email:      ")
+        user_data["Username"]     = input("* Username:   ")
+        user_data["Password"]     = hashpass(salt)
+        user_data["Creation"]     = datetime.now()
+        user_data["Modification"] = datetime.now()
+        print("========================================\n")
+        print("* Full Name: [",user_data["Name"],"]")
+        print("* Email:     [",user_data["Email"],"]")
+        print("* Username:  [",user_data["Username"],"]")
+        print("* Sector:    [",user_data["Sector"],"]")
+        print("* Password:  [",user_data["Password"],"]")
+        loop = 0
+        while loop == 0:
+            ans = input("\nConfirm (y/n)? ")
+            if ans == "y":
+                loop = 1
+                db_collection().insert_one(user_data)
+                print("Done!")
+            elif ans == "n":
+                loop = 1
+                print("Bye!")
+            else:
+                input("Wrong option! Press any key...")
+
+    except Exception as e:
+        print("\nInterrupted! ", e)
+
+result = check_credentials("ivanleoncz")
+print(result)
