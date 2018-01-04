fbb464e3b3a851ef7a27134b5ef4b14c1a3784de
ivanlmj <ivanlmj@gmail.com>
2018-01-04 16:02:37 -0600

    removing script args + improving if/else structure

--- cptool/cptool.py
+++ cptool/cptool.py
@@ -8,70 +8,64 @@ import database
 import usermgmt
 
 
-def helper():
-    print("\n[Captive Portal Tool]\n")
-    print("Options:")
-    print("    --import:  import users [.csv -> database]")
-    print("    --export:  export users [database -> .csv]")
-    print("    --search:  search user")
-    print("    --create:  create user")
-    print("    --remove:  remove user")
-    print("    --update:  update user")
-    print("    --expire:  expire session")
-    print("     --check:  checking user credentials")
-    print("      --help:  this help")
-    print("\nExample:\n    sudo python3",sys.argv[0],"--create\n")
-    print("Important:\n    Always execute it using root privileges (sudo)!\n ")
-
-
 if __name__ == "__main__":
     if os.getuid() == 0:
-        args = len(sys.argv)
-        if args == 1 or args > 2:
-            helper()
-        else:
-            if sys.argv[1] == "--import":
+        print("\n[Captive Portal Tool]\n")
+        print("1. Create User")
+        print("2. Remove User")
+        print("3. Update User")
+        print("4. Search User")
+        print("5. Import Users [csv -> database] ")
+        print("6. Export Users [database -> csv]")
+        print("7. Check User Credential")
+        print("8. Expire Active Session")
+        print("0. Exit\n")
+        opt = input("> ")
+        if opt == "1":
+            sp.call(['clear'])
+            user = usermgmt.Users()
+            user.create()
+        elif opt == "2":
+            sp.call(['clear'])
+            user = usermgmt.Users()
+            user.remove()
+        elif opt == "3":
+            sp.call(['clear'])
+            user = usermgmt.Users()
+            user.update()
+        elif opt == "4":
+            user = usermgmt.Users()
+            opt = input("\n* Full or Normal Data (f/n): ")
+            if opt == "f":
                 sp.call(['clear'])
-                oper = database.MongoDB()
-                oper.import_users()
-            elif sys.argv[1] == "--export":
+                user.search("full")
+            elif opt == "n":
                 sp.call(['clear'])
-                oper = database.MongoDB()
-                oper.export_users()
-            elif sys.argv[1] == "--search":
-                sp.call(['clear'])
-                user = usermgmt.Users()
-                opt = input("\n* Full or Normal Data (f/n): ")
-                if opt == "f":
-                    user.search("full")
-                elif opt == "n":
-                    user.search("normal")
-                else:
-                    print("\nWrong Option!\n")
-            elif sys.argv[1] == "--create":
-                sp.call(['clear'])
-                user = usermgmt.Users()
-                user.create()
-            elif sys.argv[1] == "--remove":
-                sp.call(['clear'])
-                user = usermgmt.Users()
-                user.remove()
-            elif sys.argv[1] == "--update":
-                sp.call(['clear'])
-                user = usermgmt.Users()
-                user.update()
-            elif sys.argv[1] == "--expire":
-                sp.call(['clear'])
-                oper = database.MongoDB()
-                oper.expire_session()
-            elif sys.argv[1] == "--check":
-                sp.call(['clear'])
-                user = usermgmt.Users()
-                user.check_credentials()
-            elif sys.argv[1] == "--help":
-                helper()
+                user.search("normal")
             else:
-                helper()
+                print("\nWrong Option!\n")
+        elif opt == "5":
+            sp.call(['clear'])
+            oper = database.MongoDB()
+            oper.import_users()
+        elif opt == "6":
+            sp.call(['clear'])
+            oper = database.MongoDB()
+            oper.export_users()
+        elif opt == "7":
+            sp.call(['clear'])
+            user = usermgmt.Users()
+            user.check_credentials()
+        elif opt == "8":
+            sp.call(['clear'])
+            oper = database.MongoDB()
+            oper.expire_session()
+        elif opt == "0":
+            print("\nBye!\n")
+        else:
+            print("\nWrong Option!\n")
     else:
-        helper()
+        print("\nMust have root privileges!\n    Run as.: sudo python3 cptool.py\n")
+else:
+    print("\nCannot be imported!\n    Run as.: sudo python3 cptool.py\n")
 
