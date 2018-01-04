30c3885eb114b8fe70310755382a1aa139fdd8dd
ivanlmj <ivanlmj@gmail.com>
2018-01-04 16:38:06 -0600

    ensuring execution with root privileges

--- run.py
+++ run.py
@@ -2,10 +2,15 @@
 
 """ Application main module. """
 
-from app import app
+import os
 
-app.config.from_object('config')
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=14900)
+if os.getuid() == 0:
+    from app import app
+    app.config.from_object('config')
+    if __name__ == "__main__":
+        app.run(host="0.0.0.0",port=14900)
+    else:
+        print("\nIt's a script, not a module!\n    Ex.: sudo python3 run.py\n")
+else:
+    print("\nMust have root privileges!\n    Ex.: sudo python3 run.py\n")
     
