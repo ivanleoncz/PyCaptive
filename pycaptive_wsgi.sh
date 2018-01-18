feb23d84fdf02f5c4835fdc591473811b8f8faa7
ivanlmj <ivanlmj@gmail.com>
2018-01-18 17:40:19 -0600

    configuring ip+port for standalone script (wsgi)

--- pycaptive_wsgi.sh
+++ pycaptive_wsgi.sh
@@ -2,7 +2,7 @@
 
 if [[ "$EUID" == 0 ]] ; then
 	echo -e "\nPycaptive is running (standalone: Gunicorn).\n"
-	gunicorn --preload --name gunicorn_master --user gunicorn --group gunicorn --workers 4 --pythonpath /opt/probe wsgi
+	gunicorn --preload --name gunicorn_master --bind 0.0.0.0:14900 --user gunicorn --group gunicorn --workers 4 --pythonpath /opt/probe wsgi
 else
 	echo -e "\nMust have root privileges!\n\nRun: sudo ./standalone_wsgi.py\n"
 fi
