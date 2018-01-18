7e4819a70e7b859e7e384e70006c9133eeb20548
ivanlmj <ivanlmj@gmail.com>
2018-01-18 17:09:37 -0600

    adjusting configuration of wsgi file

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,4 @@
 
 from app import app as application
 
-application.config.from_object('config')
-
-if __name__ == "__main__":
-    application.run(host="0.0.0.0",port=14901)
+application.config['MAX_CONTENT_LENGTH']=128000
