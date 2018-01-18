336d9164a2659ff40886ca44d6d015053b1921ec
ivanlmj <ivanlmj@gmail.com>
2018-01-18 17:37:07 -0600

    improving print messages for standalone scripts

--- pycaptive_flask.py
+++ pycaptive_flask.py
@@ -8,7 +8,7 @@ if os.getuid() == 0:
     from app import app
     app.config['MAX_CONTENT_LENGTH']=128000
     if __name__ == "__main__":
-        print("\nPyCaptive is running (standalone).\n")
+        print("\nPyCaptive is running (standalone: Flask).\n")
         app.run(host="0.0.0.0",port=14900)
     else:
         print("\nCannot be imported!\n\nRun: sudo ./standalone.py\n")
--- pycaptive_wsgi.sh
+++ pycaptive_wsgi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ "$EUID" == 0 ]] ; then
-	echo -e "\nPycaptive is running (standalone + WSGI).\n"
+	echo -e "\nPycaptive is running (standalone: Gunicorn).\n"
 	gunicorn --preload --name gunicorn_master --user gunicorn --group gunicorn --workers 4 --pythonpath /opt/probe wsgi
 else
 	echo -e "\nMust have root privileges!\n\nRun: sudo ./standalone_wsgi.py\n"
