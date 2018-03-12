07d6c76587b5a67d5e6205f2610d67673aa659e9
ivanlmj <ivanlmj@gmail.com>
2018-03-12 15:59:26 -0600

    converting Standalone Gunicorn script to Python3

--- pycaptive_gunicorn.py
+++ pycaptive_gunicorn.py
@@ -1,8 +1,30 @@
-#!/bin/bash
+#!/usr/bin/python3
 
-if [[ "$EUID" == 0 ]] ; then
-	echo -e "\nPycaptive is running (standalone: Gunicorn).\n"
-	gunicorn --preload --name gunicorn_master --bind 0.0.0.0:14900 --user gunicorn --group gunicorn --workers 4 --pythonpath /opt/probe wsgi
-else
-	echo -e "\nMust have root privileges!\n\nRun: sudo ./standalone_wsgi.py\n"
-fi
+""" PyCaptive Standalone (Gunicorn). 
+
+NOTICE: do not use it on production environments.
+"""
+
+__author__ = "@ivanleoncz"
+
+import os
+
+
+if os.getuid() == 0:
+    if __name__ == "__main__":
+        import subprocess as sp
+        print("\nPyCaptive is running (Standalone: Gunicorn).\n")
+        try:
+            command = ["gunicorn", "--name", "gunicorn_master", "--bind", "0.0.0.0:14900", 
+                      "--user", "root", "--group", "root", 
+                       "--workers", "4", "--pythonpath", ".", "wsgi"]
+            sp.call(command)
+        except KeyboardInterrupt:
+            print("Interrupted!")
+        except Exception as e:
+            print("Exception:", e)
+    else:
+        print("\nCannot be imported!\n\nRun: sudo ./pycaptive_gunicorn.py\n")
+else:
+    print("\nMust have root privileges!\n\nRun: sudo ./pycaptive_gunicorn.py\n")
+
