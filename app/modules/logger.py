c126979c15ae9aa5cd6074416156eb4808312173
ivanlmj <ivanlmj@gmail.com>
2018-03-16 14:39:47 -0600

    improving module DS / fixing comment + dunder var

--- app/modules/logger.py
+++ app/modules/logger.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-
-""" Module for logging configuration. """
+""" Global logging configuration. """
 
 import logging
 from logging.handlers import RotatingFileHandler
 
 def config():
     """ Setting up log configuration. """
-    conf = logging.getLogger('__name__')
-    if not conf.handlers: # this if, avoid multiple execution of log messages
+    conf = logging.getLogger(__name__)
+    if not conf.handlers: # avoids multiple log messages
         conf.setLevel(logging.ERROR)
         file_path = "/var/log/pycaptive.log"
         handler = RotatingFileHandler(file_path, maxBytes=50000000, backupCount=5)
