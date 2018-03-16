5f489a359d9e94bec9b847ec9e784ddc50701f1f
ivanlmj <ivanlmj@gmail.com>
2018-03-16 15:48:55 -0600

    fixing header structure + max chars p/line (PEP8)

--- app/modules/logger.py
+++ app/modules/logger.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 """ Global logging configuration. """
 
-import logging
 from logging.handlers import RotatingFileHandler
+
+__author__ = "@ivanleoncz"
+
+import logging
 
 def config():
     """ Setting up log configuration. """
     conf = logging.getLogger(__name__)
     if not conf.handlers: # avoids multiple log messages
         conf.setLevel(logging.ERROR)
-        file_path = "/var/log/pycaptive.log"
-        handler = RotatingFileHandler(file_path, maxBytes=50000000, backupCount=5)
+        f = "/var/log/pycaptive.log"
+        handler = RotatingFileHandler(f, maxBytes=50000000, backupCount=5)
         conf.addHandler(handler)
     return conf
