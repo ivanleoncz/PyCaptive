7ab4f229fa1a4c9696034825be10e0905521cee6
ivanlmj <ivanlmj@gmail.com>
2018-03-26 12:19:41 -0600

    restructuring module in accordance with PEP8
    - adding docstrings
    - repositioning imports

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
+""" Main application module. """
 
+from app.modules import logger
+from app.modules import scheduler
 from datetime import datetime
 from flask import Flask, request
 
-from app.modules import logger
 log = logger.config()
-
-from app.modules import scheduler
-
 app = Flask(__name__)
 
-# setting up logging of every request, except Status Code 500 (see if below...)
 @app.after_request
 def after_request(response):
+    """ Logging every request, except return code 500. """
     if response.status_code != 500:
         timestamp = datetime.now()
         log.error('[%s] %s %s %s %s %s %s',
