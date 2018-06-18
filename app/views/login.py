08f139c3e277247f55882068056de1508b403bc0
ivanlmj <ivanlmj@gmail.com>
2018-06-18 15:52:30 -0500

    removing unnecessary view
    - already redirected via NGINX (proxy)

--- app/views/login.py
+++ app/views/login.py
@@ -8,9 +8,6 @@ from app import app
 from app.modules import iptables
 from app.modules import mongodb
 
-@app.route("/", methods=['GET'])
-def f_index():
-    return redirect("/login")
 
 @app.route("/login", methods=['GET','POST'])
 def f_login():
