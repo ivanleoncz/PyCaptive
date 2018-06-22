91e9c26df62cdd1ca71352f36629d1ecb81a420d
ivanlmj <ivanlmj@gmail.com>
2018-06-22 15:02:17 -0500

    removing unnecessary variables + improving indent.

--- tools/database.py
+++ tools/database.py
@@ -9,19 +9,16 @@ import os
 
 class MongoDB:
 
-    imp = "import.csv"
-    exp = "export.csv"
-
     def connect(self):
         db_user = "mongo"
         db_pass = "mongo"
         db_addr = "127.0.0.1:27017"
-        uri = "mongodb://{0}:{1}@{2}".format(db_user,db_pass,db_addr)
-        client = MongoClient(uri,serverSelectionTimeoutMS=6000)
+        uri = "mongodb://{0}:{1}@{2}".format(db_user, db_pass, db_addr)
+        client = MongoClient(uri, serverSelectionTimeoutMS=6000)
         db = client.tjs
         return db
 
 
 if __name__ == "__main__":
-    print("\nCan't be imported!")
+    print("\nINFO: cannot be executed (module)")
 
