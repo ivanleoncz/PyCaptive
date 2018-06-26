04bd1cefe880aef3485a0056ba96d3cef6424d5a
ivanlmj <ivanlmj@gmail.com>
2018-06-26 15:46:41 -0500

    fixing output for export users script

--- tools/export_users.py
+++ tools/export_users.py
@@ -11,15 +11,15 @@ import sys
 def export_users(csv):
     """ Export users from .csv file. """
     print("[Exporting]\n")
-    print("INFO: do not import this file !!!***")
+    print("INFO: do not import this file !!!")
     mongo = database.MongoDB()
     db = mongo.connect()
     try:
         users = db.Users.find({},{"_id":0})
         if users.count() > 0:
             with open (csv, "w") as f:
+                f.write("*** DO NOT IMPORT THIS FILE ***\n")
                 for user in users:
-                    f.write("*** DO NOT IMPORT THIS FILE ***\n")
                     line = "{0},{1},{2},{3},{4}".format(
                             user["FullName"],user["Area"],user["Role"],
                             user["Email"],user["UserName"]
