f46e5449238fe0b85862e7bb3eee19a7c87b92e2
ivanlmj <ivanlmj@gmail.com>
2018-06-28 00:38:17 -0500

    improving spacing between arguments

--- app/views/login.py
+++ app/views/login.py
@@ -9,7 +9,7 @@ from app.modules import iptables
 from app.modules import mongodb
 
 
-@app.route("/login", methods=['GET','POST'])
+@app.route("/login", methods=['GET', 'POST'])
 def f_login():
     """ Processing request. """
     ipaddress = request.headers['X-Real-IP']
@@ -19,33 +19,33 @@ def f_login():
         username = request.form['username']
         password = request.form['password']
         db = mongodb.Connector()
-        login = db.login(username,password)
+        login = db.login(username, password)
         if login == 0:
-            login_record = db.add_session(username,ipaddress)
+            login_record = db.add_session(username, ipaddress)
             if login_record == 0:
                 fw = iptables.Worker()
                 allow = fw.add_rule(ipaddress)
                 if allow == 0:
-                    return redirect(url_for('f_welcome',usr=username))
+                    return redirect(url_for('f_welcome', usr=username))
                 else:
                     message = "Server Error (rule)!"
-                    return render_template("login.html",login_failed=message)
+                    return render_template("login.html", login_failed=message)
             else:
                 message = "Server Error (session)!"
-                return render_template("login.html",login_failed=message)
+                return render_template("login.html", login_failed=message)
         elif login == 1 or login == 2:
             message = "Wrong Credentials!"
-            return render_template("login.html",login_failed=message)
+            return render_template("login.html", login_failed=message)
         else:
             message = "Server Error (login)!"
-            return render_template("login.html",login_failed=message)
+            return render_template("login.html", login_failed=message)
     else:
         # 405: Method Not Allowed
         abort(405)
 
 @app.route("/welcome/<usr>")
 def f_welcome(usr):
-    login_time = datetime.now()
-    expire_time = login_time + timedelta(hours=5)
+    login_time  = datetime.now()
+    expire_time = login_time + timedelta(hours=12)
     expire_time = expire_time.strftime('%H:%M')
-    return render_template("welcome.html",username=usr,time=expire_time)
+    return render_template("welcome.html", username=usr, time=expire_time)
