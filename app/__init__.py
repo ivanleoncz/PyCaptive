dbf26711f13b8fc38b379fba66719ada806d87dd
ivanlmj <ivanlmj@gmail.com>
2018-08-11 22:05:36 -0500

    renaming Flask Config file

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ log = logger.config()
 from app.modules import scheduler
 
 app = Flask(__name__)
-app.config.from_pyfile('settings.cfg')
+app.config.from_pyfile('flask_settings.cfg')
 
 @app.after_request
 def after_request(response):
