1b7ae18a3dce7482e6e69dbdfad104bb2d8888d8
ivanlmj <ivanlmj@gmail.com>
2018-09-26 16:07:21 -0500

    transforming script into README.md file
    - still improving...

--- deploy/run.sh
+++ deploy/run.sh
@@ -1,99 +1,35 @@
-#!/bin/bash
+### Create PyCaptive Group and User
+/usr/sbin/groupadd pycaptive -g 14090
+/usr/sbin/useradd pycaptive -s /usr/sbin/nologin -c "PyCaptive Account" -u 14090 -g 14090
 
-PYCAPTIVE_USR="pycaptive"
-INSTALL_TIMESTAMP="`/bin/date`"
+### Setting up Log Directory
 
-function user_setup {
+/bin/mkdir /var/log/pycaptive
+/bin/chmod 770 /var/log/pycaptive
+/bin/chown pycaptive:pycaptive /var/log/pycaptive
+/bin/echo "PyCaptive Deploy at: `/bin/date`" > /var/log/pycaptive/pycaptive.log
+/bin/chown pycaptive:pycaptive /var/log/pycaptive/pycaptive.log
+/bin/chmod 660 /var/log/pycaptive/pycaptive.log
 
-    COMMENT="Account for running PyCaptive"
-    PC_SHELL="/usr/sbin/nologin"
+### IPTABLES
+/bin/mkdir /etc/iptables
+/bin/bash /opt/Pycaptive/deploy/iptables/firewall_setup.sh
+/bin/cp /opt/PyCaptive/deploy/rules.v4 /etc/iptables/
 
-    /usr/sbin/groupadd $PYCAPTIVE_USR -g 14090
-    /usr/sbin/useradd $PYCAPTIVE_USR -s $PC_SHELL -c "$COMMENT" -u 14090 -g 14090
+### Logrotate
+/bin/cp /opt/PyCaptive/deploy/logrotate/pycaptive /etc/logrotate.d/
+/bin/chmod 444 /etc/logrotate.d/pycaptive
 
-}
+### Sudoers
+/bin/cp /opt/PyCaptive/deploy/sudoers.d/pycaptive /etc/sudoers.d
+/bin/chmod 444 /etc/sudoers.d/pycaptive
 
-function log_setup {
+### Nginx
+/bin/cp /opt/PyCaptive/deploy/nginx/pycaptive /etc/nginx/sites-available/
+/bin/ln -s /etc/nginx/sites-available/pycaptive /etc/nginx/sites-enabled/pycaptive
+/bin/chmod 444 /etc/nginx/sites-enabled/pycaptive
 
-    LOGDIR="/var/log/pycaptive"
-    LOGFILE="$LOGDIR/pycaptive.log"
+### Supervisor
+/bin/cp /opt/PyCaptive/deploy/supervisor/pycaptive /etc/supervisor/conf.d/pycaptive
+/usr/bin/supervisorctl restart
 
-    mkdir $LOGDIR
-    chown $PYCAPTIVE_USR:$PYCAPTIVE_USR $LOGDIR
-    chmod 770 $LOGDIR
-    echo "PyCaptive Deploy started at: $INSTALL_TIMESTAMP" > $LOGFILE
-    chown $PYCAPTIVE_USR:$PYCAPTIVE_USR $LOGFILE
-    chmod 660 $LOGFILE
-    cp -p logrotate/pycaptive /etc/logrotate.d/
-
-}
-
-function sudoers_setup {
-
-    SUDOERS_FILE="/etc/sudoers.d/pycaptive"
-    CONNTRACK_BIN="/usr/sbin/conntrack"
-    IPTABLES_BIN="/sbin/iptables"
-
-    echo "$PYCAPTIVE_USR ALL=(root:root) NOPASSWD:$IPTABLES_BIN" > $SUDOERS_FILE
-    echo "$PYCAPTIVE_USR ALL=(root:root) NOPASSWD:$CONNTRACK_BIN" >> $SUDOERS_FILE
-    chmod 0440 $SUDOERS_FILE
-
-}
-
-function nginx_setup {
-
-    NGINX_DIR="/etc/nginx"
-
-    cp nginx/pycaptive "$NGINX_DIR/sites-available/"
-    ln -s "$NGINX_DIR/sites-available/pycaptive" "$NGINX_DIR/sites_enabled/pycaptive"
-
-}
-
-function supervisor_setup {
-    
-    SUPERVISOR_DIR="/etc/supervisor/conf.d"
-    
-    cp supervisor/pycaptive "$SUPERVISOR_DIR/conf.d/"
-}
-
-function check_binnaries {
-
-    packages=`dpkg-query -l | grep ^'.i' | awk '{print $2}' | \
-              grep 'python3-pip\|nginx$\|iptables$\|conntrack$\|supervisor$\|mongo-org'`
-
-    package_counter=0
-    package_executable=0
-    for pkg in $packages; do
-	echo "Checking package: $pkg"
-        ((package_counter++))
-        for bin in `whereis $pkg`; do
-            if [[ $bin =~ "bin" ]]; then
-		echo "- binnary found: $bin"
-                if [ -x $bin ]; then
-		    echo "- executable: yes"
-                    ((package_executable++))
-		fi
-            fi
-        done
-    done
-
-    if [ $package_counter -q $package_executable ]; then
-        echo "Final status: OK"
-    else
-	echo "Final status: NOK"
-	echo -e "INFO: please, review installation for the following packages"
-	echo "iptables\nconntrack\|nginx\|python3-pip\|supervisor\|mongod"
-	exit
-    fi
-
-}
-
-
-if [ "$UID" == 0 ]
-then
-    user_setup
-    log_setup
-    sudoers_setup
-else
-    echo "You don't have root privileges!"
-fi
