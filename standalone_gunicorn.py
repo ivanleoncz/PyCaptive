792d7ec8698779792a83a9ce4a19de9efb3430c8
ivanlmj <ivanlmj@gmail.com>
2018-10-02 11:39:14 -0500

    PyCaptive has his own user for its execution

--- standalone_gunicorn.py
+++ standalone_gunicorn.py
@@ -6,28 +6,21 @@ from app.custom_settings import HOST, PORT
 
 __author__ = "@ivanleoncz"
 
-import os
-
 host_port = HOST + ":" + str(PORT)
 
-if os.getuid() == 0:
-    if __name__ == "__main__":
-        import subprocess as sp
-        print("\nPyCaptive is running: Standalone Mode (Gunicorn)\n")
-        print("\033[1;34mINFO\033[1;m: don't use it on production environments!\n")
-        try:
-            command = ["gunicorn", "--name", "gunicorn_master",
-                                   "--bind", host_port,
-                                   "--user", "root",
-                                   "--group", "root",
-                                   "--workers", "4",
-                                   "--pythonpath", ".", "wsgi"]
-            sp.call(command)
-        except KeyboardInterrupt:
-            print("Interrupted!")
-        except Exception as e:
-            print("Exception:", e)
-else:
-    print("\n\033[1;31mERROR\033[1;m: Must have root privileges!")
-    print("Run:\n\t$ sudo ./pycaptive_gunicorn.py")
-
+if __name__ == "__main__":
+    import subprocess as sp
+    print("\nPyCaptive is running: Standalone Mode (Gunicorn)\n")
+    print("\033[1;34mINFO\033[1;m: don't use it on production environments!\n")
+    try:
+        command = ["gunicorn", "--name", "gunicorn_master",
+                               "--bind", host_port,
+                               "--user", "root",
+                               "--group", "root",
+                               "--workers", "4",
+                               "--pythonpath", ".", "wsgi"]
+        sp.call(command)
+    except KeyboardInterrupt:
+        print("Interrupted!")
+    except Exception as e:
+        print("Exception:", e)
--- standalone_werkzeug.py
+++ standalone_werkzeug.py
@@ -6,21 +6,13 @@ from app.custom_settings import HOST, PORT
 
 __author__ = "@ivanleoncz"
 
-import os
-
-
-if os.getuid() == 0:
-    if __name__ == "__main__":
-        from app import app
-        print("\nPyCaptive is running: Standalone Mode  (Werkzeug)\n")
-        print("\033[1;34mINFO\033[1;m: don't use it on production environments!\n")
-        try:
-            app.run(host=HOST, port=PORT)
-        except KeyboardInterrupt:
-            print("Interrupted!")
-        except Exception as e:
-            print("Exception:", e)
-else:
-    print("\n\033[1;31mERROR\033[1;m: Must have root privileges!")
-    print("Run:\n\t$ sudo ./pycaptive_werkzeug.py")
-    
+if __name__ == "__main__":
+    from app import app
+    print("\nPyCaptive is running: Standalone Mode  (Werkzeug)\n")
+    print("\033[1;34mINFO\033[1;m: don't use it on production environments!\n")
+    try:
+        app.run(host=HOST, port=PORT)
+    except KeyboardInterrupt:
+        print("Interrupted!")
+    except Exception as e:
+        print("Exception:", e)
