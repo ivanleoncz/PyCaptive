879ab6a5e11d5636d0bcac5be76025a18281d294
ivanlmj <ivanlmj@gmail.com>
2018-10-02 12:27:01 -0500

    adjusting docstrings and console output

--- standalone.py
+++ standalone.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-""" PyCaptive: Standalone Mode (Werkzeug) """
+""" PyCaptive: Standalone Mode """
 
 from app.custom_settings import HOST, PORT
 
@@ -8,8 +8,8 @@ __author__ = "@ivanleoncz"
 
 if __name__ == "__main__":
     from app import app
-    print("\nPyCaptive is running: Standalone Mode  (Werkzeug)\n")
-    print("\033[1;34mINFO\033[1;m: don't use it on production environments!\n")
+    print("\nPyCaptive Standalone Mode  (Werkzeug)\n")
+    print("\033[1;34mINFO\033[1;m: designed for test purposes only.\n")
     try:
         app.run(host=HOST, port=PORT)
     except KeyboardInterrupt:
