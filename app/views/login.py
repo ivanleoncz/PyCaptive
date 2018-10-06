3f10e3bcdb6cc40db90f6e6e1262aecca10f2fba
ivanlmj <ivanlmj@gmail.com>
2018-10-06 14:09:08 -0500

    processing UserAgent data for session purposes
    - database module must be adjusted for processing
    user_data dictionary, in order to store individual data
    for each document (key/value pairs)
    - session checking must be adjusted for delivering
    user data, after successful login, when redirection to
    /welcome resource is performed (where user data will
    be processed by Jinja2

--- app/views/login.py
+++ app/views/login.py
@@ -3,6 +3,7 @@
 
 from flask import abort, redirect, render_template, request, url_for
 from datetime import datetime
+from user_agents import parse
 
 from app import app, log
 from app.modules import iptables
@@ -25,12 +26,30 @@ def f_login():
         log.error('[%s] %s %s %s %s', ts, "/login", "GET", client_ip, "OK")
         return render_template("login.html")
     elif request.method == 'POST':
+        ua = parse(request.headers.get('User-Agent'))
+        # checking device presence (True/False)
+        devices = {}
+        devices["pc"] = ua.is_pc
+        devices["mobile"] = ua.is_mobile
+        devices["tablet"] = ua.is_tablet
+        # functions for determining Unknown brand or family for a device
+        brand = lambda x: "Unknown" if x is None else ua.device.brand
+        family = lambda x: "Unknown" if x is "Other" else ua.device.family
+        # building user_data (device detection via list comprehension )
+        user_data = {}
+        user_data["device"] = [k for k,v in devices.items() if v == True][0]
+        user_data["brand"] = brand(ua.device.brand)
+        user_data["family"] = family(ua.device.family)
+        user_data["os"] = ua.os.family
+        user_data["browser"] = ua.browser.family
+        user_data["ip"] = client_ip
+        # username & password
         username = request.form['username']
         password = request.form['password']
         db = mongodb.Connector()
         login = db.login(username, password)
         if login == 0:
-            login_record = db.add_session(username, client_ip)
+            login_record = db.add_session(username, user_data)
             if login_record == 0:
                 fw = iptables.Worker()
                 allow = fw.add_rule(client_ip)
