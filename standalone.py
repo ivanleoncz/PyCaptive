33d7c262f32f92db8658aca982f681722647da61
ivanlmj <ivanlmj@gmail.com>
2018-10-15 14:07:16 -0500

    roll back standalone config from settings file

--- standalone.py
+++ standalone.py
@@ -2,11 +2,9 @@
 
 """ PyCaptive: Standalone Mode """
 
-from app.global_settings import HOST, PORT
-
-
 if __name__ == "__main__":
     from app import app
+    from app.pycaptive_settings import HOST, PORT
     print("\n --------------------------------------------")
     print("\n PyCaptive Standalone Mode\n")
     print(" \033[1;34mINFO\033[1;m: I was designed for test purposes only.\n")
