ebfc7178dc1f9d1cb6b8681706dd0153fbbe1b95
ivanlmj <ivanlmj@gmail.com>
2018-10-17 17:40:11 -0500

    adapting scheduler module to new logging schema
    - removing manual timestamp (log format already has it)
    - adding pycaptive_settings setup (TIME_INTERVAL)
    - removing module definition for TIME_INTERVAL

--- app/modules/scheduler.py
+++ app/modules/scheduler.py
@@ -9,19 +9,14 @@ the rules associated with the IP addresses from the result.
 
 """
 
-from app import log
+from app import log, TIME_INTERVAL
 from app.modules import mongodb
 from app.modules import iptables
 from apscheduler.schedulers.background import BackgroundScheduler
-from datetime import datetime
-
-# expressed in seconds
-INTERVAL_TIME=60
 
 
 def expirer():
     """ Searching/cleaning expired sessions """
-    ts = datetime.now()
     db = mongodb.Connector()
     sessions = db.expire_sessions() # querying expired sessions
     if type(sessions) == list:
@@ -29,13 +24,11 @@ def expirer():
             fw = iptables.Worker()
             counter = fw.del_rules(sessions) # deleting rules
             if type(counter) != int:
-                log.error('[%s] %s %s %s',
-                  ts, "scheduler", "expirer", "FAIL_IPTABLES")
+                log.error('%s %s %s', "scheduler", "expirer", "FAIL_IPTABLES")
     else:
-        log.error('[%s] %s %s %s',
-          ts, "scheduler", "expirer", "FAIL_MONGODB")
+        log.error('%s %s %s', "scheduler", "expirer", "FAIL_MONGODB")
 
 
 sched = BackgroundScheduler(daemon=True)
-sched.add_job(expirer, 'interval', seconds=INTERVAL_TIME)
+sched.add_job(expirer, 'interval', seconds=TIME_INTERVAL)
 sched.start()
