c5005efe729cd05561ea9f8e25136089af2fcde5
ivanlmj <ivanlmj@gmail.com>
2018-10-17 14:58:44 -0500

    adjusting log configuration + improving log format
    - adding two log handlers, according with pycaptive_settings definition
    - improving log format, with timestamp and level printed by default

--- app/modules/logger.py
+++ app/modules/logger.py
@@ -1,6 +1,8 @@
 """ Global logging configuration. """
 
-from logging.handlers import TimedRotatingFileHandler
+from logging.handlers import TimedRotatingFileHandler as tr_fh
+
+from app import LOG_FILE, LOG_ROTATE, LOG_ROTATE_WHEN, LOG_ROTATE_COUNT
 
 __author__ = "@ivanleoncz"
 
@@ -8,10 +10,19 @@ import logging
 
 def config():
     """ Setting up log configuration. """
-    log_f = "/var/log/pycaptive/pycaptive.log"
+
     conf = logging.getLogger(__name__)
     if not conf.handlers: # avoids multiple log messages
-        conf.setLevel(logging.ERROR)
-        handler = TimedRotatingFileHandler(log_f, when='midnight', backupCount=52)
+        conf.setLevel(logging.INFO)
+        log_form = logging.Formatter('%(asctime)s [%(levelname)s]\t%(message)s')
+        handler = None
+        if LOG_ROTATE == True:
+            #  logging module will rotate the log files
+            handler = tr_fh(LOG_FILE,
+                       when=LOG_ROTATE_WHEN, backupCount=LOG_ROTATE_COUNT)
+        elif LOG_ROTATE == False:
+            # no rotation will be performed, leaving rotation to OS tool
+            handler = logging.FileHandler(LOG_FILE)
+        handler.setFormatter(log_form)
         conf.addHandler(handler)
     return conf
