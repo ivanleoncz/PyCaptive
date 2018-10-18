05f9ff20f3a4d2dc7f54b92299e3edbe3d5b4a1f
ivanlmj <ivanlmj@gmail.com>
2018-10-18 17:11:18 -0500

    adjusting mongodb projection (returning all data)

--- app/modules/mongodb.py
+++ app/modules/mongodb.py
@@ -45,9 +45,7 @@ class Connector:
         client = self.connect()
         db = client.tjs
         collection = db.Sessions
-        session_data = collection.find_one(
-                {"IpAddress":ipaddress},
-                {"UserName":1, "IpAddress":1, "_id":0})
+        session_data = collection.find_one( {"IpAddress":ipaddress},{"_id":0} )
         if session_data:
             return session_data
         else:
