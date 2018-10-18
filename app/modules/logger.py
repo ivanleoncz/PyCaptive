26b595ae1c4ab6a47c24771a9538baf1fe48c592
ivanlmj <ivanlmj@gmail.com>
2018-10-18 10:29:55 -0500

    improving comments and function formmating

--- app/modules/logger.py
+++ app/modules/logger.py
@@ -12,17 +12,20 @@ def config():
     """ Setting up log configuration. """
 
     conf = logging.getLogger(__name__)
-    if not conf.handlers: # avoids multiple log messages
+
+    # avoids multiple log messages
+    if not conf.handlers:
         conf.setLevel(logging.INFO)
         log_form = logging.Formatter('%(asctime)s [%(levelname)s]\t%(message)s')
         handler = None
         if LOG_ROTATE == True:
-            #  logging module will rotate the log files
+            # configuring log rotation (see pycaptive_settings.py)
             handler = tr_fh(LOG_FILE,
                        when=LOG_ROTATE_WHEN, backupCount=LOG_ROTATE_COUNT)
         elif LOG_ROTATE == False:
-            # no rotation will be performed, leaving rotation to OS tool
+            # no log rotation will be performed, leaving to OS (logorotate)
             handler = logging.FileHandler(LOG_FILE)
         handler.setFormatter(log_form)
         conf.addHandler(handler)
+
     return conf
