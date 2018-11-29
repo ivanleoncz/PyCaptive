49e57d01c3c8cd331fbda614967f1e1f43afaa0b
ivanlmj <ivanlmj@gmail.com>
2018-11-29 16:32:45 -0600

    structuring module with class + updating import
    - now: GLOBAL_CONF

--- app/modules/checksys.py
+++ app/modules/checksys.py
@@ -1,52 +1,51 @@
 
-
 import socket
 import subprocess as sp
 import sys
+from app import TEST, IPTABLES, CONNTRACK, DB_ADDR, DB_PORT
+from app import GLOBAL_CONF as v
 
 
-binaries = [IPTABLES, CONNTRACK]
-services = None
-
-if TEST:
-    services = { "mongodb": (DB_ADDR, DB_PORT) }
-else:
-    try:
-        result = sp.check_output(["ip", "addr", "show", LAN])
-        result = result.split()
-        ipaddr = result[result.index('inet') + 1].split('/')[0]
-        services = {
-                        "nginx": (ipaddr, 14901),
-                        "mongodb": (DB_ADDR, DB_PORT)
-                    }
-    except Exception:
-        print("ERROR: check pycaptive_settings.py (IPTABLES section)")
-        sys.exit(1)
+class Components():
 
 
-def check_binaries(binaries):
-    """ Check existence of binaries. """
-    for b in binaries:
-        result = sp.call(["which", b], stderr=sp.DEVNULL, stdout=sp.DEVNULL)
-        result_bin = b.split('/')[-1]
-        if result == 0:
-            print("{0:10} {1:12}: {2}".format("binary", result_bin, "OK"))
+    def __init__(self):
+        self._binaries = (IPTABLES, CONNTRACK)
+        self._services = None
+
+        if TEST:
+            self._services = {"mongodb": (DB_ADDR, DB_PORT)}
         else:
-            print("{0:10} {1:12}: {2}".format("binary", result_bin, "NOK"))
+            self._services = {
+                             "nginx": (v["LAN_IP"], v["NGINX_REDIR_GUNICORN"]),
+                             "mongodb": (DB_ADDR, DB_PORT)
+            }
 
 
-def check_service(services):
-    """ Check status of a network service. """
-    for k,v in services.items():
-        s = socket.socket()
-        try:
-            s.connect((v[0], int(v[1])))
-            print("{0:10} {1:12}: {2}".format("service", k, "OK"))
-        except Exception:
-            print("{0:10} {1:12}: {2}".format("service", k, "NOK"))
-        finally:
-            s.close()
+    def binaries(self):
+        """ Check existence of binaries. """
+        results = dict()
+        for b in self._binaries:
+            result = sp.call(["which", b], stderr=sp.DEVNULL, stdout=sp.DEVNULL)
+            result_bin = b.split('/')[-1]
+            if result == 0:
+                results[result_bin] = 0
+            else:
+                results[result_bin] = 1
+        return results
 
 
-check_binaries(binaries)
-check_service(services)
+    def services(self):
+        """ Check status of network services. """
+        # TODO: must ensure that this is working....
+        results = dict()
+        for k,v in self._services.items():
+            s = socket.socket()
+            try:
+                s.connect((v[0], int(v[1])))
+                results[k] = 0
+            except Exception:
+                results[k] = 1
+            finally:
+                s.close()
+        return results
