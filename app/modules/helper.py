1ffe1da9c90df196d2af0832767ccb39f8f234cf
ivanlmj <ivanlmj@gmail.com>
2019-01-15 15:54:32 -0600

    adding exception when LAN_NIC has no IP config

--- app/modules/helper.py
+++ app/modules/helper.py
@@ -1,9 +1,16 @@
 """ Provides helper functions. """
 
+import subprocess as sp
+import sys
+
+
 def get_nic_ip(nic):
     """ Get IP address from a NIC."""
-    import subprocess as sp
     result = sp.check_output(["ip", "addr", "show", nic])
     result = result.decode().split()
-    ipaddr = result[result.index('inet') + 1].split('/')[0]
-    return ipaddr
+    try:
+        ipaddr = result[result.index('inet') + 1].split('/')[0]
+        return ipaddr
+    except Exception:
+        print("Helper ERROR: no network configuration was detected for LAN_NIC")
+        sys.exit(1)
