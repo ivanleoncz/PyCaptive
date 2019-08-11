15c809cfe1f3e7ed8f614cfc5095aa01cb69b294
ivanlmj <ivanlmj@gmail.com>
2019-08-10 21:51:38 -0500

    removing unnecessary script
    - user has to define the IP address of the NIC which will listen
    requests for PyCaptive

--- app/helpers.py
+++
@@ -1,9 +0,0 @@
-
-
-def get_nic_ip(nic):
-    """ Get IP address from a NIC."""
-    import subprocess as sp
-    result = sp.check_output(["ip", "addr", "show", nic])
-    result = result.decode().split()
-    ipaddr = result[result.index('inet') + 1].split('/')[0]
-    return ipaddr
