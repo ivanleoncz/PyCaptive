88181d8b95683cc444ad8ed667d9263d375cb445
ivanleoncz <ivanlmj@gmail.com>
2020-10-31 13:45:15 -0600

    Redefining app entrypoint configuration + running
    - pycaptive.ini file has been defined (values yet to be implemented
    across the application)
    - pycaptive.yaml was deprecated and therefore removed
    - pycaptive.ini detection and loading (on app.config object) was also implemented
    - unnecessary after-request configuration was removed
    - wsgi file was added for Systemd/Gunicorn purposes, but its also
    available for manual execution

--- app/__init__.py
+++ app/__init__.py
@@ -1,27 +1,57 @@
 #!/usr/bin/python3
 """ Main application module. """
+import configparser
+import os
+import logging
+import sys
+
 from flask import Flask, abort, redirect, request, render_template, session
+
+# Logging
+FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+logging.basicConfig(format=FORMAT)
+logger = logging.getLogger('pycaptive')
+logger.setLevel(logging.INFO)
+
+
+# PyCaptive configuration via .ini file
+logger.info("detecting and loading pycaptive.ini")
+config = configparser.ConfigParser()
+paths = ('/etc/pycaptive/pycaptive.ini', 'app/pycaptive.ini', 'pycaptive.ini')
+for ini in paths:
+    if os.path.isfile(ini):
+        logger.info(f"configuration file {os.path.abspath(ini)}")
+        config.read(ini)
+        break
+
+
+# Generating dict based on pycaptive.ini for loading into Flask app.config
+if config.sections():
+    ini_dict = {}
+    for section in config:
+        for opt in list(config[section]):
+            if section in ini_dict:
+                ini_dict[section][opt] = config[section][opt]
+            else:
+                ini_dict[section] = {opt: config[section][opt]}
+else:
+    logger.error("no pycaptive.ini file was found. Aborting!")
+    sys.exit()
+
 
 # Flask Setup
 app = Flask(__name__)
 app.config['DEBUG'] = False
 app.config['SECRET_KEY'] = os.urandom(24)
-
-# TODO: Logging Setup (???)
+app.config['PYCAPTIVE'] = ini_dict
 
 
 # Scheduler
 from app.modules import scheduler
 
-@app.after_request
-def after_request(response):
-    """ Setting response headers for every request. """
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    response.headers["Pragma"] = "no-cache"
-    response.headers["Expires"] = "0"
-    response.headers['Cache-Control'] = 'public, max-age=0'
-    return response
+# Views
+from app.views import login, welcome
 
-
-from app.views import login
-from app.views import welcome
+@app.route("/test")
+def f_test():
+    return "PyCaptive is running!"
